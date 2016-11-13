269d9a7fd6c972810a5d14a19087478a27717df5
TianzeXu <tianzexu96@gmail.com>
2016-11-13 12:38:13 +0800

    Hotel的Test 这回全过了

--- MockSrc/HotelTester/HotelTest.java
+++ MockSrc/HotelTester/HotelTest.java
@@ -6,37 +6,55 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
-import OrdersMock.OrderPO;
-import OrdersMock.OrdersMock;
-import PromotionsMock.PromotionsMock;
-import PromotionsMock.PromotionsPO;
+import EvaluateMock.EvaluatePO;
+import HotelsMock.HotelPO;
+import HotelsMock.HotelMock;
+import HotelsMock.ResultMessage;
+
 
 public class HotelTest {
 	
 	@Test
-	public void AddHotelTest(){
-		PromotionsMock p = new PromotionsMock();
-		PromotionsPO promotionPO1 = p.getPromotion("南京饭店");
-		assertEquals(promotionPO1.getHotel(),"南京饭店");
+	public void findHoteltTest(){
+		HotelMock hotelMock = new HotelMock();
+		HotelsMock.HotelPO po = hotelMock.findHotel("金陵饭店");
+		assertEquals(po.getName(), "金陵饭店");	
 	}
 	
 	@Test
-	public void getHistoryHotelTest(){
-		OrdersMock m =new OrdersMock();
-		ArrayList <OrderPO> list = m. getHotelHistory ("李安");
-		for(OrderPO cell:list){
-			assertEquals(cell.getUserNameString(),"李安");
-		}
+	public void findHoteltTest2(){
+		HotelMock hotelMock2 = new HotelMock();
+		HotelsMock.HotelPO po2 = hotelMock2.findHotel("苏宁环球");
+		assertEquals(po2.getName(), "苏宁环球");
+	}
+	@Test
+	public void addHotelTest1(){
+		HotelMock hotelMock3 = new HotelMock();
+		ResultMessage  r = hotelMock3.addHotel(null);
+		assertEquals(r,ResultMessage.failure);
+	}
+	@Test
+	public void addHotelTest2(){
+		HotelMock hotelMock4 = new HotelMock();
+		HotelPO p = new HotelPO("南京饭店", null, null, null, null, null);
+		ResultMessage  r = hotelMock4.addHotel(p);
+		assertEquals(r,ResultMessage.success);
+	}
+	@Test
+	public void addEvaluationTest1(){
+		HotelMock hotelMock5 = new HotelMock();
+		ResultMessage  r = hotelMock5.addHotelEvaluation(null);
+		assertEquals(r,ResultMessage.failure);
+	}
+	@Test
+	public void addEvaluationTest2(){
+		HotelMock hotelMock6 = new HotelMock();
+		EvaluatePO evaluatePO = new EvaluatePO("南京饭店", null, 0, 0, null);
+		ResultMessage  r = hotelMock6.addHotelEvaluation(evaluatePO);
+		assertEquals(r,ResultMessage.success);
 	}
 	
-	public void getOrderHistoryTest(){
-		OrdersMock m =new OrdersMock();
-		ArrayList <OrderPO> list = m. getOrderHistory ("李安","金陵饭店");
-		for(OrderPO cell:list){
-			assertEquals(cell.getUserNameString(),"李安");
-			assertEquals(cell.getHotelNameString(),"金陵饭店");
-		}
-	}
+
 	
 	
 	
