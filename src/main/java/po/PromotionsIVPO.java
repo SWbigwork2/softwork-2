a484fbefa04b10a7a9011e6acf3a50e7acd89b56
MasterQiu <1360610714@qq.com>
2016-12-02 11:33:43 +0800

    策略4

--- src/main/java/po/PromotionsIVPO.java
+++ src/main/java/po/PromotionsIVPO.java
@@ -3,12 +3,16 @@ package po;
 
 
 public class PromotionsIVPO extends PromotionsPO{
-	   
-	    public PromotionsIVPO(int type,String hotel,String introduction,double discount){
+	   String companyname;
+	   public String getcompanyname(){
+		   return companyname;
+	   }
+	    public PromotionsIVPO(int type,String hotel,String introduction,String companyname,double discount){
 	    	this.type=type;
 	    	this.hotel=hotel;
 	    	this.introduction=introduction;
 	    	this.discount=discount;
+	    	this.companyname=companyname;
 	    }
 	  
 }
