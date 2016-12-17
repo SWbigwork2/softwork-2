f2c8b01c104e92b8e2212bfed5c35feec76bfdfb
xiao-king <ykky32085@163.com>
2016-12-17 19:07:10 +0800

    改成了暗码，修改了评价

--- src/main/java/Usersblimpl/UserPoVoTran.java
+++ src/main/java/Usersblimpl/UserPoVoTran.java
@@ -69,7 +69,7 @@ public class UserPoVoTran {
     
 	private static MemberInformationVO memberPoToVo(MemberInformationPO member){
 		MemberInformationVO memberInformationVO=new MemberInformationVO(member.getId()
-				,  member.getName(),member.getPassword(), member.getCredit()
+				,  member.getName(),darkToWhite(member.getPassword()), member.getCredit()
 				, member.getContactInformation(), Integer.toString(member.getLevel()),
 				member.getSpecial(), member.getVipType().name());
 		return memberInformationVO;
@@ -78,13 +78,13 @@ public class UserPoVoTran {
 	private static MemberInformationPO memberVoToPo(MemberInformationVO member){
 		
 		MemberInformationPO memberPO=new MemberInformationPO(member.getUserId(), member.getName(),
-				member.getPassword(), member.getContactInformation(), 
+				whiteToDark(member.getPassword()), member.getContactInformation(), 
 				member.getCredit(), VipType.valueOf(member.getVipType()), member.getSpecial(), Integer.valueOf(member.getLevel()));
 		return memberPO;
 	}
 	
 	private static StaffVO staffPoToVo(StaffPO staff){
-		StaffVO staffVO=new StaffVO(staff.getId(), staff.getPassword()
+		StaffVO staffVO=new StaffVO(staff.getId(), darkToWhite(staff.getPassword())
 				, staff.getName(), staff.getHotelName());
 	    return staffVO;
 	}
@@ -92,27 +92,50 @@ public class UserPoVoTran {
 	
 	private static StaffPO staffVoToPo(StaffVO staff){
 		StaffPO staffPO=new StaffPO(staff.getUserId(), staff.getName()
-				, staff.getPassword(), staff.getHotelName());
+				, whiteToDark(staff.getPassword()), staff.getHotelName());
 	    return staffPO;
 	}
 	
 	private static MarketerVO marketerPoToVo(MarketerPO markrter){
-		MarketerVO markrterVO=new MarketerVO(markrter.getId(), markrter.getPassword(), markrter.getName());
+		MarketerVO markrterVO=new MarketerVO(markrter.getId(), darkToWhite(markrter.getPassword()), markrter.getName());
 		return markrterVO;
 	}
 	
 	private static MarketerPO marketerVoToPo(MarketerVO markrter){
-		MarketerPO markrterPO=new MarketerPO(markrter.getUserId(), markrter.getName(), markrter.getPassword());
+		MarketerPO markrterPO=new MarketerPO(markrter.getUserId(), markrter.getName(), whiteToDark(markrter.getPassword()));
 	    return markrterPO;
 	}
 	
 	
 	private static ManagerVO managerPoToVo(ManagerPO manager){
-		ManagerVO managerVO=new ManagerVO(manager.getId(), manager.getName(), manager.getPassword());
+		ManagerVO managerVO=new ManagerVO(manager.getId(), manager.getName(), darkToWhite(manager.getPassword()));
 		return managerVO;
 	}
 	private static ManagerPO managerVoToPo(ManagerVO manager){
-		ManagerPO managerPO=new ManagerPO(manager.getUserId(), manager.getName(), manager.getPassword());
+		ManagerPO managerPO=new ManagerPO(manager.getUserId(), manager.getName(), whiteToDark(manager.getPassword()));
 		return managerPO;
+	}
+	
+	public    static String whiteToDark(String password){
+		String darkPassword="";
+		for(int i=0;i<password.length();i++){
+			char c=password.charAt(i);
+			String temp=Integer.toString(c);
+			while(temp.length()<3){
+				temp="0"+temp;
+			}
+			darkPassword=darkPassword+temp;
+		}
+		return darkPassword;
+	}
+	
+	public  static String darkToWhite(String darkPassword){
+		String whitePassword="";
+		for(int i=0;i<darkPassword.length();i+=3){
+			int temp=Integer.parseInt(darkPassword.substring(i, i+3));
+			
+			whitePassword=whitePassword+(char)temp;
+		}
+		return whitePassword;
 	}
 }
