4bf0e56f0f03f447da552aefb13a70e0af5bb129
MasterQiu <1360610714@qq.com>
2016-12-23 20:09:46 +0800

    营销人员main的注释

--- src/main/java/view/marketer/MarketerMain.java
+++ src/main/java/view/marketer/MarketerMain.java
@@ -41,6 +41,9 @@ public class MarketerMain extends Application {
 	private OrdersService ordersService;
 private RemoteHelper remoteHelper;
 	
+/**
+ * 连接到服务器
+ */
 public void linkToServer(){
 	final String ip = "localhost";
 	try{
@@ -64,6 +67,9 @@ public void linkToServer(){
 	
 	
 	}
+	/**
+	 * 加载订单列表
+	 */
 	public void showList(){
 		 loader=new FXMLLoader();
 		 SplitPane pane = null;
@@ -81,6 +87,10 @@ public void linkToServer(){
 			}
 		marketerBorderPane.setCenter(pane);
 	}
+	/**
+	 * @param vo
+	 * 设置memberVO
+	 */
 	public void setMemberVo(LoginVo vo){
 		userService = new UserControllerblimpl();
 		this.marketer =(MarketerVO) userService.find(vo.getId(), UserType.marketer);
@@ -88,10 +98,16 @@ public void linkToServer(){
 		
 		
 	}
+	/**
+	 * 登出
+	 */
 	public void logout(){
 		primaryStage.close();
 		loginStage.showAndWait();
 	}
+	/**
+	 * 展示营销人员主界面
+	 */
 	public void showMain(){
 		loginStage.close();
 		this.primaryStage.setTitle("营销人员主界面");
@@ -140,6 +156,9 @@ public void linkToServer(){
 		}
 		
 	}
+	/**
+	 * 跳转到显示网站策略
+	 */
 	public void movetoLookPromotions(){
 		 
 		 try {
@@ -151,6 +170,10 @@ public void linkToServer(){
 			e.printStackTrace();
 		}
 	 }
+	/**
+	 * @param fxml
+	 * @return  加载界面
+	 */
 	private   Pane loadPane(String fxml) {
 		// TODO Auto-generated method stub
 		  loader=new FXMLLoader();
@@ -165,28 +188,30 @@ public void linkToServer(){
 			return null;
 		}
 	}
-	 public void movetoPromotionsv(){
+	 /**
+	 *   跳转到制定商圈策略
+	 */
+	public void movetoPromotionsv(){
 	    	try {
 	    		
 	    		AnchorPane promotionsvPane=new AnchorPane();
 				 promotionsvPane=(AnchorPane)loadPane("Promotionsv.fxml");
 				 promotionsvPane.setPrefSize(556, 598);
-//				 promotionsvPane.setMaxSize(556, 598);
-//				 promotionsvPane.setMinSize(556, 598);
 				mainPane.getItems().set(1, promotionsvPane);
 			} catch (Exception e) {
 				// TODO: handle exception
 				e.printStackTrace();
 			}
 	    }
-	 public void movetoPromotionsvi(){
+	 /**
+	 *   跳转到会员等级策略界面
+	 */
+	public void movetoPromotionsvi(){
 	    	try {
 	    		
 	    		AnchorPane promotionsviPane=new AnchorPane();
 				 promotionsviPane=(AnchorPane)loadPane("Promotionsvi.fxml");
 				 promotionsviPane.setPrefSize(556, 598);
-//				 promotionsviPane.setMaxSize(556, 598);
-//				 promotionsviPane.setMinSize(556, 598);
 				mainPane.getItems().set(1, promotionsviPane);
 				
 			} catch (Exception e) {
@@ -194,21 +219,25 @@ public void linkToServer(){
 				e.printStackTrace();
 			}
 	    }
-	 public void movetoPromotionsii2(){
+	 /**
+	 * 跳转到制定节日特惠策略界面
+	 */
+	public void movetoPromotionsii2(){
 	    	try {
 	    		
 	    		AnchorPane promotionsii2Pane=new AnchorPane();
 				 promotionsii2Pane=(AnchorPane)loadPane("Promotionsii2.fxml");
 				 promotionsii2Pane.setPrefSize(556, 598);
-//				 promotionsii2Pane.setMaxSize(556, 598);
-//				 promotionsii2Pane.setMinSize(556, 598);
 				 mainPane.getItems().set(1, promotionsii2Pane);
 			} catch (Exception e) {
 				// TODO: handle exception
 				e.printStackTrace();
 			}
 	    }
-	 public void movetoAddPromotions(){
+	 /**
+	 * 跳转到添加策略主界面
+	 */
+	public void movetoAddPromotions(){
 		 try {					 
 			 loader=new FXMLLoader();
 			 loader.setLocation(MarketerMain.class.getResource("MarketerPromotionsframe.fxml"));
@@ -221,18 +250,11 @@ public void linkToServer(){
 			e.printStackTrace();
 		}
 	 }
-	 public void movetoDeletePromotions(){
-		 try {
-			 System.out.println(2);
-			AnchorPane DeletePromotionsPane=new AnchorPane();
-			DeletePromotionsPane=(AnchorPane)loadPane("DeleteMarketPromotions.fxml");
-			marketerBorderPane.setCenter(DeletePromotionsPane);
-		} catch (Exception e) {
-			e.printStackTrace();
-			// TODO: handle exception
-		}
-	 }
-	 public void movetoAddCredit(){
+	 
+	 /**
+	 *  跳转到充值信用界面
+	 */
+	public void movetoAddCredit(){
 		 try{
 			 loader =new FXMLLoader();
 			 loader.setLocation(MarketerMain.class.getResource("AddCredit.fxml"));
@@ -244,7 +266,10 @@ public void linkToServer(){
 			 e.printStackTrace();
 		 }
 	 }
-	 public void movetoInformation(){
+	 /**
+	 * 跳转营销人员个人信息
+	 */
+	public void movetoInformation(){
 		 try{
 			 loader =new FXMLLoader();
 			 loader.setLocation(MarketerMain.class.getResource("MarketerInformationUi.fxml"));
@@ -259,11 +284,16 @@ public void linkToServer(){
 		    e.printStackTrace();
 		 }
 	 }
+	
 	 public void setMarketer(String id){
 		 UserService userService=new UserControllerblimpl();
 		 this.marketer=(MarketerVO)userService.find(id, UserType.marketer);
 	 }
-	 public void movetoRevampPassword(){
+	 
+	 /**
+	 * 跳转到修改密码界面
+	 */
+	public void movetoRevampPassword(){
 		 try{
 			 loader =new FXMLLoader();
 			 loader.setLocation(MarketerMain.class.getResource("revampPasswordUi.fxml"));
@@ -278,7 +308,13 @@ public void linkToServer(){
 	 }
 
 
-	 public void showWaningInformation(AlertType type,
+	 /**
+	 * @param type
+	 * @param headerText
+	 * @param contentText
+	 * 提示框
+	 */
+	public void showWaningInformation(AlertType type,
 				String headerText,String contentText){
 			 Alert alert = new Alert(type);
 		     alert.setTitle("来自系统的提示");
